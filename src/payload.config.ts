ab400b7410e398776a09c61f07b04a4c886e2632
fahmi-mac <f.hidayah@pixilapps.com>
2025-09-20 22:01:21 +0700

    remove cloud plugin

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -37,7 +37,7 @@ export default buildConfig({
   sharp,
   plugins: [
     payloadCloudPlugin(),
-    getCloudStoragePlugin(),
+    // getCloudStoragePlugin(),
     // storage-adapter-placeholder
   ],
 })
